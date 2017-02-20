f9d68f91041762099c629cc8c72af859cbcae69b
jeroendedauw <jeroendedauw@gmail.com>
2017-02-20 15:02:35 +0100

    PP with albin

--- tests/GildedRose/Tests/GildedRoseTest.php
+++ tests/GildedRose/Tests/GildedRoseTest.php
@@ -2,7 +2,19 @@
 
 namespace GildedRose\Tests;
 
+use GildedRose\Item;
+use GildedRose\Program;
+
 class GildedRoseTest extends \PHPUnit_Framework_TestCase
 {
+  public function testUpdateQuality() {
+    $items = array(new Item(array( 'name' => '+5 Dexterity Vest','sellIn' => 10,'quality' => 20)));
+
+    $program = new Program($items);
+
+    $program->UpdateQuality();
+
+    $this->assertEquals(10 - 1, $items[0]->sellIn);
+  }
 }
 
