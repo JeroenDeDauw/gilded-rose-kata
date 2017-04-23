9b1881a770d8a6e21c60263cc71088248fd7cfb7
Jeroen De Dauw <jeroendedauw@gmail.com>
2017-04-23 11:21:15 +0200

    PP with Tobias J

--- main.php
+++ main.php
@@ -4,3 +4,14 @@ require_once "vendor/autoload.php";
 require_once "src/GildedRose/Program.php";
 
 GildedRose\Program::main();
+
+/*
+HELLO
+                                              Name -  SellIn - Quality
+							     +5 Dexterity Vest -       9 -      19
+                                         Aged Brie -       1 -       1
+                            Elixir of the Mongoose -       4 -       6
+                        Sulfuras, Hand of Ragnaros -       0 -      80
+         Backstage passes to a TAFKAL80ETC concert -      14 -      21
+                                Conjured Mana Cake -       2 -       5
+*/
--- tests/GildedRose/Tests/GildedRoseTest.php
+++ tests/GildedRose/Tests/GildedRoseTest.php
@@ -2,7 +2,21 @@
 
 namespace GildedRose\Tests;
 
+use GildedRose\Item;
+use GildedRose\Program;
+
 class GildedRoseTest extends \PHPUnit_Framework_TestCase
 {
+
+	public function testUpdateQuality() {
+		$item = new Item( ['name' => "foobar",'sellIn' => 10,'quality' => 20] );
+
+		$program = new Program( [ $item ] );
+
+		$program->UpdateQuality();
+
+		$this->assertSame( 19, $item->quality );
+	}
+
 }
 
