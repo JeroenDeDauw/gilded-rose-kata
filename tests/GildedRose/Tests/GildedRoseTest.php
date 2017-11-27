ba6eedf1fc852a0443f3a4a09f6b8bc0e2184507
Jeroen De Dauw <jeroendedauw@gmail.com>
2017-11-27 11:43:37 +0100

    PP with Adam

--- tests/GildedRose/Tests/GildedRoseTest.php
+++ tests/GildedRose/Tests/GildedRoseTest.php
@@ -2,7 +2,34 @@
 
 namespace GildedRose\Tests;
 
+use GildedRose\Item;
+use GildedRose\Program;
+
 class GildedRoseTest extends \PHPUnit_Framework_TestCase
 {
-}
+	public function testQualityIsNeverBelowZero() {
+		$item = new Item([ 'name' => "Normal item",'sellIn' => 10,'quality' => 0 ]);
 
+		( new Program( [ $item ] ) )->UpdateQuality();
+
+		$this->assertSame( 0, $item->quality );
+    }
+
+	public function testAgedBrieQualityIncreases()
+	{
+		$item = new Item(['name' => "Aged Brie", 'sellIn' => 10, 'quality' => 0]);
+
+		(new Program([$item]))->UpdateQuality();
+
+		$this->assertSame(1, $item->quality);
+	}
+
+	public function testQualityIsNeverOver50()
+	{
+		$item = new Item(['name' => "Aged Brie", 'sellIn' => 10, 'quality' => 50]);
+
+		(new Program([$item]))->UpdateQuality();
+
+		$this->assertSame(50, $item->quality);
+	}
+}
