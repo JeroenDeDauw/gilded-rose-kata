531697e96b917a335c7902ee46b46afd815eb310
Jeroen De Dauw <jeroendedauw@gmail.com>
2018-12-05 17:54:45 +0100

    PP w/ Rosalie

--- src/GildedRose/Program.php
+++ src/GildedRose/Program.php
@@ -65,27 +65,36 @@ class Program
                 }
             }
 
-            if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros") {
-                $this->items[$i]->sellIn = $this->items[$i]->sellIn - 1;
-            }
+            $this->sulfurasUpdate($this->items[$i]);
 
-            if ($this->items[$i]->sellIn < 0) {
-                if ($this->items[$i]->name != "Aged Brie") {
-                    if ($this->items[$i]->name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if ($this->items[$i]->quality > 0) {
-                            if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros") {
-                                $this->items[$i]->quality = $this->items[$i]->quality - 1;
-                            }
-                        }
-                    } else {
-                        $this->items[$i]->quality = $this->items[$i]->quality - $this->items[$i]->quality;
-                    }
-                } else {
-                    if ($this->items[$i]->quality < 50) {
-                        $this->items[$i]->quality = $this->items[$i]->quality + 1;
-                    }
-                }
-            }
+            $this->sellInLessThan0qualityUpdate($this->items[$i]);
         }
     }
+
+   public function sulfurasUpdate($itemUpdating) {
+        if ($itemUpdating->name != "Sulfuras, Hand of Ragnaros") {
+            $itemUpdating->sellIn = $itemUpdating->sellIn - 1;
+        }
+   }
+
+   public function sellInLessThan0qualityUpdate(Item $item) {
+       if ($item->sellIn < 0) {
+           if ($item->name != "Aged Brie") {
+               if ($item->name != "Backstage passes to a TAFKAL80ETC concert") {
+                   if ($item->quality > 0) {
+                       if ($item->name != "Sulfuras, Hand of Ragnaros") {
+                           $item->quality = $item->quality - 1;
+                       }
+                   }
+               } else {
+                   $item->quality = $item->quality - $item->quality;
+               }
+           } else {
+               if ($item->quality < 50) {
+                   $item->quality = $item->quality + 1;
+               }
+           }
+       }
+   }
+    
 }
