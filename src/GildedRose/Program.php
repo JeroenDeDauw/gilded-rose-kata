54c7dbe927adb40c11920dda8863a7c216c82af9
Jeroen De Dauw <jeroendedauw@gmail.com>
2018-12-24 11:38:28 +0100

    PP with tim

--- src/GildedRose/Program.php
+++ src/GildedRose/Program.php
@@ -39,15 +39,15 @@ class Program
     public function UpdateQuality()
     {
         for ($i = 0; $i < count($this->items); $i++) {
-            if ($this->items[$i]->name != "Aged Brie" && $this->items[$i]->name != "Backstage passes to a TAFKAL80ETC concert") {
-                if ($this->items[$i]->quality > 0) {
-                    if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros") {
+            if ($this->items[$i]->name != "Aged Brie" && $this->items[$i]->name != "Backstage passes to a TAFKAL80ETC concert") { // Quality decreases with time.
+                if ($this->items[$i]->quality > 0) { // Quality is still above zero, can be decreased.
+                    if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros") { // Quality of Legendary does not decrease.
                         $this->items[$i]->quality = $this->items[$i]->quality - 1;
                     }
                 }
-            } else {
+            } else { // Quality increases with time.
                 if ($this->items[$i]->quality < 50) {
-                    $this->items[$i]->quality = $this->items[$i]->quality + 1;
+                    $this->items[$i]->quality += 1;
 
                     if ($this->items[$i]->name == "Backstage passes to a TAFKAL80ETC concert") {
                         if ($this->items[$i]->sellIn < 11) {
@@ -78,7 +78,7 @@ class Program
                             }
                         }
                     } else {
-                        $this->items[$i]->quality = $this->items[$i]->quality - $this->items[$i]->quality;
+                        $this->items[$i]->quality = 0;
                     }
                 } else {
                     if ($this->items[$i]->quality < 50) {
@@ -86,6 +86,8 @@ class Program
                     }
                 }
             }
+
+            //
         }
     }
 }
--- tests/GildedRose/Tests/GildedRoseTest.php
+++ tests/GildedRose/Tests/GildedRoseTest.php
@@ -2,9 +2,64 @@
 
 namespace GildedRose\Tests;
 
+use GildedRose\Item;
+use GildedRose\Program;
 use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
+    public function assertQualityAfterOneDay(int $expectedQuality, Item $item) {
+        $program = new Program([$item]);
+        $program->UpdateQuality();
+        $this->assertSame($expectedQuality, $item->quality);
+    }
+
+    public function testNormalItem()
+    {
+        $this->assertQualityAfterOneDay(
+            19,
+            new Item(array('name' => '+5 Dexterity Vest','sellIn' => 10,'quality' => 20))
+        );
+    }
+
+	public function testAgedBrie()
+	{
+        $this->assertQualityAfterOneDay(
+            1,
+            new Item(array('name' => 'Aged Brie','sellIn' => 2,'quality' => 0))
+        );
+	}
+
+	public function testSulfuras()
+	{
+		$this->assertQualityAfterOneDay(
+			80,
+			new Item(array('name' => 'Sulfuras, Hand of Ragnaros','sellIn' => 0,'quality' => 80))
+		);
+	}
+
+	public function testBackstagePasses()
+	{
+		$this->assertQualityAfterOneDay(
+			21,
+            new Item(array(
+                'name' => 'Backstage passes to a TAFKAL80ETC concert',
+                'sellIn' => 15,
+                'quality' => 20
+            ))
+		);
+	}
+
+	public function testBackstagePassesIncreaseInQualityByTwo()
+	{
+		$this->assertQualityAfterOneDay(
+			22,
+			new Item(array(
+				'name' => 'Backstage passes to a TAFKAL80ETC concert',
+				'sellIn' => 10,
+				'quality' => 20
+			))
+		);
+	}
 }
 
